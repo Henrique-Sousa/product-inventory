7bc3b1db613b43f5177134e7fb5e44980da51560
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-18 18:53:52 -0300

    implement product view get method

--- src/br/com/henriquesousa/productinventory/controller/ProductViewController.java
+++ src/br/com/henriquesousa/productinventory/controller/ProductViewController.java
@@ -4,6 +4,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
+import java.sql.*;
+import br.com.henriquesousa.productinventory.model.*;
 
 @WebServlet("/product")
 public class ProductViewController extends HttpServlet {
@@ -12,8 +14,28 @@ public class ProductViewController extends HttpServlet {
         throws ServletException, IOException {
         
         response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
-        out.println("<h1>Product details placholder</h1>");
+
+        try {
+            String sid = request.getParameter("id");
+            Long id = Long.parseLong(sid);
+
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/productinventory", "productmanager", "123456");
+            ProductDAO productDAO = new ProductDAO(connection);
+
+            Product product = productDAO.findById(id); 
+
+            connection.close();
+            
+            out.println("<h1>Product View</h1>");
+            out.println("<p>Name: " + product.getName() + "</p>");
+            out.println("<p>Description: " + product.getDescription() + "</p>");
+            out.println("<p>Price: " + product.getPrice() + "</p>");
+            out.println("<p>Quantity: " + product.getQuantity() + "</p>");
+
+
+        } catch(SQLException e) {
+            out.println(e);
+        }
     }
 }
