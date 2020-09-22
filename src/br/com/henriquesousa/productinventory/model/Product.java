65dfe1e970f0923ed65d2cb773f9f522bc7b5902
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-21 23:29:44 -0300

    add getter and setter for product id

--- src/br/com/henriquesousa/productinventory/model/Product.java
+++ src/br/com/henriquesousa/productinventory/model/Product.java
@@ -6,7 +6,13 @@ public class Product {
     private String name, description;
     private double price;
     private long quantity, id;
- 
+
+    public long getId() {
+        return this.id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getName() {
         return this.name;
     }
