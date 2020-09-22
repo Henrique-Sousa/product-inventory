298d49cc9f26c0bc6123bf768e48ea8fa2f80991
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-22 00:38:55 -0300

    remove try catch since the methods are already declared with "throws" keyword

--- src/br/com/henriquesousa/productinventory/model/ProductDAO.java
+++ src/br/com/henriquesousa/productinventory/model/ProductDAO.java
@@ -10,37 +10,32 @@ public class ProductDAO extends DAO<Product> {
 
     @Override
     public void save(Product product) throws SQLException {
-        try {
-            String query = "INSERT INTO products (name, description, price, quantity) VALUES (?, ?, ?, ?)";
-            PreparedStatement statement = this.connection.prepareStatement(query);
-            statement.setString(1, product.getName());
-            statement.setString(2, product.getDescription());
-            statement.setDouble(3, product.getPrice());
-            statement.setLong(4, product.getQuantity());
-            statement.execute();
-        } catch(SQLException e) { 
-            throw e;
-        }
+        String query = "INSERT INTO products (name, description, price, quantity) VALUES (?, ?, ?, ?)";
+        PreparedStatement statement = this.connection.prepareStatement(query);
+        statement.setString(1, product.getName());
+        statement.setString(2, product.getDescription());
+        statement.setDouble(3, product.getPrice());
+        statement.setLong(4, product.getQuantity());
+        statement.execute();
     }
 
     @Override
     public Product findById(long id) throws SQLException {
-        Product product = new Product();
-        try {
-            String query = "SELECT product_id, name, description, price, quantity FROM products WHERE product_id = ?";
-            PreparedStatement statement = this.connection.prepareStatement(query);
-            statement.setLong(1, id);
-            ResultSet result = statement.executeQuery();
-            result.next();
+        String query = "SELECT product_id, name, description, price, quantity FROM products WHERE product_id = ?";
+        PreparedStatement statement = this.connection.prepareStatement(query);
+        statement.setLong(1, id);
+        ResultSet result = statement.executeQuery();
+        if (result.next() == false) {
+            return null;
+        } else {
+            Product product = new Product();
             product.setId(result.getLong("product_id"));
             product.setName(result.getString("name"));
             product.setDescription(result.getString("description"));
             product.setPrice(result.getDouble("price"));
             product.setQuantity(result.getLong("quantity"));
-        } catch(SQLException e) { 
-            throw e;
+            return product;
         }
-        return product;
     }
 
 }
