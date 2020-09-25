3589350bf4cf51d72911686a83e825c77c6ed849
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-25 14:22:23 -0300

    feature: controller and view to list all products

--- src/br/com/henriquesousa/productinventory/controller/ProductListController.java
+++ src/br/com/henriquesousa/productinventory/controller/ProductListController.java
@@ -4,6 +4,10 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
+import br.com.henriquesousa.productinventory.model.ProductDAO;
+import br.com.henriquesousa.productinventory.util.*;
+import java.sql.*;
+import java.util.*;
 
 @WebServlet("/products")
 public class ProductListController extends HttpServlet {
@@ -11,9 +15,16 @@ public class ProductListController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         
-        response.setContentType("text/html");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/productinventory", "productmanager", "123456");
+            ProductDAO productDAO = new ProductDAO(connection);
+            List products = productDAO.findAll();
+            request.setAttribute("products", products);
+            Utils.forward("/product_list.jsp", request, response);
+        } catch (SQLException e) {
+            request.setAttribute("error", "Internal error");
+            Utils.forward("/error.jsp", request, response);
+        }
 
-        PrintWriter out = response.getWriter();
-        out.println("<h1> Product list placholder </h1>");
     }
 }
---
+++ src/webapp/product_list.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>  
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Product List</title>
+    </head>
+    <body>
+        <h2>Products</h2>
+        <div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                <tbody>
+                <c:forEach items="${products}" var="product">
+                    <tr>    
+                        <td>${product.id}</td>
+                        <td>${product.name}</td>
+                        <td>${product.description}</td>
+                        <td>${product.price}</td>
+                        <td>${product.quantity}</td>
+                    </tr>
+                </c:forEach>
+                </tbody>
+            </table>
+
+        </div>
+    </body>
+</html>
