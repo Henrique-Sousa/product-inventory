446353046075b1d45e1783a3236c93c18e959a77
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-29 23:58:09 -0300

    feat: add ability to read input from create product form and write them on screen

--- src/br/com/henriquesousa/productinventory/controller/ProductCreateController.java
+++ src/br/com/henriquesousa/productinventory/controller/ProductCreateController.java
@@ -11,6 +11,7 @@ import br.com.henriquesousa.productinventory.util.*;
 @WebServlet("/product/create")
 public class ProductCreateController extends HttpServlet {
 
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         Utils.forward("/product_create.jsp", request, response);
@@ -18,6 +19,49 @@ public class ProductCreateController extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        this.doGet(request, response);
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        
+        try {
+            long id = Long.parseLong(request.getParameter("id"));
+            out.println(id);
+        } catch(NumberFormatException e) {
+            request.setAttribute("error", "Missing: id");
+            Utils.forward("/error.jsp", request, response);
+        }
+
+        String name = request.getParameter("name"); 
+        if (name != "") {
+            out.println(name);
+        } else {
+            request.setAttribute("error", "Missing: name");
+            Utils.forward("/error.jsp", request, response);
+        }
+
+        String description = request.getParameter("description"); 
+        if (description != "") {
+            out.println(description);
+        } else {
+            request.setAttribute("error", "Missing: description");
+            Utils.forward("/error.jsp", request, response);
+        }
+
+        try {
+            double price = Double.parseDouble(request.getParameter("price"));
+            out.println(price);
+        } catch(NumberFormatException e) {
+            request.setAttribute("error", "Missing: price");
+            Utils.forward("/error.jsp", request, response);
+        }
+
+        try {
+            long quantity = Long.parseLong(request.getParameter("quantity"));
+            out.println(quantity);
+        } catch(NumberFormatException e) {
+            request.setAttribute("error", "Missing: quantity");
+            Utils.forward("/error.jsp", request, response);
+        }
+
     }
 }
